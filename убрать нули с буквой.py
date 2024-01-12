fe4e30b2ef3ef71b1b74adb3fbe486f4c8653d50
Вячеслав Опарин <119162900+Slavkula@users.noreply.github.com>
2024-01-12 17:49:00 +0700

    Update убрать нули с буквой.py

--- убрать нули с буквой.py
+++ убрать нули с буквой.py
@@ -5,7 +5,7 @@ def remove_zeroes(doc_path):
     doc = Document(doc_path)
     for para in doc.paragraphs:
         for run in para.runs:
-            for i in range(10, 131):
+            for i in range(0, 131):  
                 num = i / 10
                 run.text = re.sub(rf'({num:.2f})м\b', lambda m: f'{m.group(1)[:-1]}м', run.text)
     doc.save('updated_' + doc_path)

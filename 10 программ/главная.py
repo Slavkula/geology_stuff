7f0b5fc00dda9d5fc50bfc5a301518a28f6e7790
Вячеслав Опарин <119162900+Slavkula@users.noreply.github.com>
2024-01-27 03:00:19 +0700

    Create главная.py

---
+++ 10 программ/главная.py
@@ -0,0 +1,73 @@
+import tkinter as tk
+import subprocess
+
+def open_card_script():
+    subprocess.call(['python', 'карточки.py'])
+
+window = tk.Tk()
+window.title("Открытие скрипта")
+
+open_card_button = tk.Button(window, text="Создание карточек", command=open_card_script, font=('Arial', 20), width=30)
+open_card_button.pack()
+
+def open_card_script():
+    subprocess.call(['python', 'белые нули.py'])
+
+open_card_button = tk.Button(window, text="Белые нули в ведомость", command=open_card_script, font=('Arial', 20), width=30)
+open_card_button.pack()
+
+def open_card_script():
+    subprocess.call(['python', 'сделать csv.py'])
+
+open_card_button = tk.Button(window, text="Сделать .csv из экселя", command=open_card_script, font=('Arial', 20), width=30)
+open_card_button.pack()
+
+def open_card_script():
+    subprocess.call(['python', 'вкладки2.py'])
+
+open_card_button = tk.Button(window, text="Создание вкладок2", command=open_card_script, font=('Arial', 20), width=30)
+open_card_button.pack()
+
+def open_card_script():
+    subprocess.call(['python', 'карточки3.py'])
+
+open_card_button = tk.Button(window, text="Создание карточек3", command=open_card_script, font=('Arial', 20), width=30)
+open_card_button.pack()
+
+def open_card_script():
+    subprocess.call(['python', 'вкладки3.py'])
+
+open_card_button = tk.Button(window, text="Создание вкладок3", command=open_card_script, font=('Arial', 20), width=30)
+open_card_button.pack()
+
+def open_card_script():
+    subprocess.call(['python', 'карточки4.py'])
+
+open_card_button = tk.Button(window, text="Создание карточек4", command=open_card_script, font=('Arial', 20), width=30)
+open_card_button.pack()
+
+def open_card_script():
+    subprocess.call(['python', 'вкладки4.py'])
+
+open_card_button = tk.Button(window, text="Создание вкладок4", command=open_card_script, font=('Arial', 20), width=30)
+open_card_button.pack()
+
+def open_card_script():
+    subprocess.call(['python', 'карточки5.py'])
+
+open_card_button = tk.Button(window, text="Создание карточек5", command=open_card_script, font=('Arial', 20), width=30)
+open_card_button.pack()
+
+def open_card_script():
+    subprocess.call(['python', 'карточки5.py'])
+
+open_card_button = tk.Button(window, text="Создание карточек5", command=open_card_script, font=('Arial', 20), width=30)
+open_card_button.pack()
+
+def open_card_script():
+    subprocess.call(['python', 'карточки6.py'])
+
+open_card_button = tk.Button(window, text="Создание карточек6", command=open_card_script, font=('Arial', 20), width=30)
+open_card_button.pack()
+
+window.mainloop()

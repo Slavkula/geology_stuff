416aea2984b2f04f7963353cfc3995bedc0d40f4
Вячеслав Опарин <119162900+Slavkula@users.noreply.github.com>
2024-01-30 03:01:26 +0700

    Update добавить нули в ведомость.py

--- 10 программ/добавить нули в ведомость.py
+++ 10 программ/добавить нули в ведомость.py
@@ -78,13 +78,4 @@ run_button.pack()
 result_label = tk.Label(root, text="", font=('Arial', 22))
 result_label.pack()
 
-text = """
-1. Копируешь оригинальный файл и работаешь в нём.
-2. Выбираешь файл, столбцы и строки куда добавить нули.
-3. Нажимаешь 'Выполнить'. Может занять время.
-4. Повторяешь для следующего блока.
-"""
-text_label = tk.Label(root, text=text, font=("Arial", 20))
-text_label.pack()
-
 root.mainloop()
